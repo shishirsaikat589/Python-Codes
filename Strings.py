18b5dee12ee67b20022d8c4147c2106ea76fae79
Shishir Saikat <95974768+shishirsaikat589@users.noreply.github.com>
2026-08-19 01:34:43 +0600

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,25 @@
+# txt = "Hello, World!"
+# print(txt[2:5])
+# print(txt.upper())
+#
+# name = "Python"
+# print(f"I love {name}")
+
+# 1. Creating Strings
+msg = "Hello"
+name = "Alice"
+
+full_msg = msg + ", " + name
+print(full_msg)
+
+f_string = f"{msg}, {name}!"
+print(f_string)
+
+print(len(msg))
+print(name.upper())
+print(name.lower())
+
+print(name.replace("Alice", "Bob"))
+print("e" in msg)
+
+
