c381bec64ca47bd67ce8c1d7c8e7ed345c84f704
Shishir Saikat <95974768+shishirsaikat589@users.noreply.github.com>
2026-08-19 16:48:29 +0600

    Add files via upload

---
+++ M0dules.py
@@ -0,0 +1,11 @@
+import math
+import random
+
+number = 25
+
+square_root = math.sqrt(number)
+random_number = random.randint(1, 100)
+
+print("Square root:", square_root)
+print("Random number:", random_number)
+print("Rounded value:", math.ceil(3.14))
